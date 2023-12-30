b7f03f46fc487f4a3d28d45f2860b798030530d7
Vadim Smuk <Smuk_vadim@mail.ru>
2023-12-31 02:34:11 +0500

    первая задача

---
+++ Desktop/DZ/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число a: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 7 == 0 && a % 23 ==0 )
+{
+Console.Write ("да");
+}
+else 
+{
+Console.Write ("нет");
+}
